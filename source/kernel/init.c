56deb64a19bbfa4505155f2fc490ef8ebe66fbb4
Gil Mendes <gil00mendes@gmail.com>
2015-11-16 12:04:32 +0000

    Guarda o endereço de memoria da lista de tags do Initium
    E adicionei alguns main todos para o projeto

--- source/kernel/init.c
+++ source/kernel/init.c
@@ -7,13 +7,39 @@
 #include <compiler.h>
 #include <initium.h>
 
+/** Indica ao Initium que é um Kernel suportado */
 INITIUM_IMAGE(INITIUM_IMAGE_SECTIONS || INITIUM_IMAGE_LOG);
+
+/** Endereço da lista de tags do Initium */
+static initium_tag_t *initium_tag_list __init_text;
 
 /**
  * Ponto de entrada do kernel.
+ *
+ * @param magic     Numero magico do Initium.
+ * @param tags      Apontador para a lista de tags.
  */
-__init_text void kmain()
+__init_text void kmain(uint32_t magic, initium_tag_t *tags)
 {
+    /** Guarda o endereço a lista de tags. */
+    initium_tag_list = tags;
+
     /* Cria um ciclo infinito */
     while(1);
+
+    /** TODO: iniciar o kdb */
+
+    /** TODO: iniciar as funcs. basicas da consola */
+
+    /** TODO: inicia o as funções basicas do log */
+
+    /** TODO: inicia as funções basicas da CPU */
+
+    /** TODO: inicia a gestão de memoria */
+
+    /** TODO: configura a consola por completo */
+
+    /** TODO: finaliza a inicialização da CPU */
+
+    /** TODO: (...) */
 }
